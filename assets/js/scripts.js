a9fcf7728d87104d3c6061d3c81b9f1ae6815904
Davi A Alves <daviih2000@gmail.com>
2023-11-17 17:28:27 -0300

    feat: Implementar lógica de conversão de moeda

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,17 @@
 const convertButton = document.querySelector(".convert-button");
 
 function convertCurrency() {
-  const inputCurrencyValue = document.querySelector(".input-currency").value;
+	const inputCurrencyValue = document.querySelector(".input-currency").value;
+	const currencyValueToBeConverted = document.querySelector(
+		".currency-value-to-be-converted"
+	);
+	const currencyValueConverted = document.querySelector(".currency-value");
 
-  console.log(inputCurrencyValue);
+	const dolarToday = 4.9;
+
+	const convertedValue = inputCurrencyValue / dolarToday;
+
+	console.log(convertedValue);
 }
 
 convertButton.addEventListener("click", convertCurrency);
